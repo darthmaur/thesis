54ab4ca7bf4e01971e7cd54f2f85996f1ef1e7df
Mauricio Hidalgo <mauridamaya@gmail.com>
2015-05-13 21:40:45 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<body>
+	<div class="cover">
+		<?php include 'cover.php' ?>
+	</div>
+	<div class="synopsis">
+		Sinopsis
+	</div>
+	<div class="dedication">
+		Dedicatoria
+	</div>
+	<div class="acknowledge">
+		Agradecimientos
+	</div>
+	<div class="content">
+		Contenido
+	</div>
+	<div class="index">
+
+	</div>
+	<div class="chapter">
+	</div>
+</body>
+
